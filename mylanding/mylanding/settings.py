d9f1c05ad81c12977fdc11d42bc5ad6e88e7d7ce
Mukhammadali Mustafaev <mukhammadalimustafaev@MukhammdalisAir.lan>
2025-05-15 01:01:04 +0500

    Fix: move sys.path after BASE_DIR definition

--- mylanding/mylanding/settings.py
+++ mylanding/mylanding/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-sys.path.append(str(BASE_DIR / "mylanding"))
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.append(str(BASE_DIR / "mylanding"))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
