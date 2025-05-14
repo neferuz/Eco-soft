5b91926551f03a9347106d870ac9a212a11e0e40
Mukhammadali Mustafaev <mukhammadalimustafaev@MukhammdalisAir.lan>
2025-05-14 20:56:52 +0500

    Fix: correct DJANGO_SETTINGS_MODULE path

--- mylanding/mylanding/wsgi.py
+++ mylanding/mylanding/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylanding.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylanding.mylanding.settings')
 
 application = get_wsgi_application()
