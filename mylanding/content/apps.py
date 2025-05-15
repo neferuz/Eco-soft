0dd0d9eb94db2fd209281db4303309487f13bc28
Mukhammadali Mustafaev <mukhammadalimustafaev@MukhammdalisAir.lan>
2025-05-15 17:39:09 +0500

    Fix: corrected app path in ContentConfig

--- mylanding/content/apps.py
+++ mylanding/content/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class ContentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'content'
+    name = 'mylanding.content'
     verbose_name = "⚙️ Настройки сайта"
